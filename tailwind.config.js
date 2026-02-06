c216ea64d9fca10444c5ebf61b4ac6199e7a3773
dongha22 <tethys731@gmail.com>
2026-02-06 09:42:36 +0900

    chore: setup tailwind npm, add supabase client, cleanup unused deps

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./*.html",
+    "./*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
